ea53e35fc6a1538d5951e800e45eea2f7dbfe468
Devin Pastoor <devin.pastoor@gmail.com>
2015-07-08 16:58:42 -0400

    update to include routes so can hit .../profile/<name>

---
+++ app/components/Profile.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Router from 'react-router';
+
+class Profile extends React.Component {
+  getInitialState() {
+    return {
+      notes: [],
+      bio: {},
+      repos: []
+    };
+  }
+  render () {
+    return (
+        <div className="row">
+          <div className="col-md-4">
+            User Profile Component
+          </div>
+          <div className="col-md-4">
+            Repos Component
+          </div>
+          <div className="col-md-4">
+            Notes Component
+          </div>
+        </div>
+    )
+  }
+}
+
+export default Profile;
--- app/config/routes.js
+++ app/config/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Main from '../components/Main';
 import Home from '../components/Home';
+import Profile from '../components/Profile.js';
 import {Router, Route, DefaultRoute} from 'react-router';
 
 
 export default (
 	//whenever we hit the root of our app, render the main component
 	<Route name="app" path="/" handler={Main}>
+		<Route name="profile" path="profile/:username" handler={Profile} />
 		// default route to go to if no other routes match request
 		<DefaultRoute handler={Home} />
 	</Route>
